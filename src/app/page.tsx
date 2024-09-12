f3e2ed72ebcb4257a6bf101b1454406389d86e60
tronica21 <97139228+tronica21@users.noreply.github.com>
2024-09-12 16:49:34 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section>
+      <div className='py-24'>
+        <div className='container'>
+          <div className='text-3xl font-bold'>Next Starter</div>
+        </div>
+      </div>
+    </section>
+  )
+}
